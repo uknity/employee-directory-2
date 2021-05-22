1153abc2973c9ebf9730fbce0ca0b1305442edab
Liz Emerson <uknity@gmail.com>
2021-05-22 17:16:39 -0400

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,15 +5,24 @@ import headerimg from "./headerimg.jpg";
 //function to create header
 function Header() {
 	return (
-		<header>
-			<div className="jumbotron jumbotron-fluid headerStyle">
+		<div>
+			<div className="jumbotron jumbotron-fluid" style={{
+        height: '400px',
+        paddingBottom: '0px',
+        color: '#f5426c',
+        textAlign: 'center',
+        marginBottom: '30px',
+        backgroundImage: `url(${headerimg})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}>
 				<div className="container">
 					<h1 className="display-4">Employee Directory</h1>
 
 					<p className="lead">Begin typing an employee name to allow search</p>
 				</div>
 			</div>
-		</header>
+		</div>
 	);
 }
 
