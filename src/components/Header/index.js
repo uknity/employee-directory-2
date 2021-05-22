077606979422820f73f6588f7ae87da31b271983
Liz Emerson <uknity@gmail.com>
2021-05-22 17:03:04 -0400

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,17 +6,8 @@ import "./style.css";
 function Header() {
 	return (
 		<header>
-			<div
-				className="jumbotron jumbotron-fluid headerStyle"
-				// style={{
-				// 	backgroundImage: `url(${headerimg})`,
-				// 	backgroundSize: "cover",
-				// 	backgroundRepeat: "no-repeat",
-				// 	height: "400px",
-            
-				// }}
-			>
-				<div className="container headerContainer">
+			<div className="jumbotron jumbotron-fluid headerStyle">
+				<div className="container">
 					<h1 className="display-4">Employee Directory</h1>
 
 					<p className="lead">Begin typing an employee name to allow search</p>
